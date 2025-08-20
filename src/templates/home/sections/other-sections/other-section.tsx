ded37e4bec30197ee49484e7d23f21f421ace0f1
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-08-20 18:52:06 -0300

    Scale imagems

--- src/templates/home/sections/other-sections/other-section.tsx
+++ src/templates/home/sections/other-sections/other-section.tsx
@@ -9,24 +9,54 @@ export function OtherSection() {
 
       <div className="grid grid-cols-4 gap-3 mt-16 h-96">
         <div className="bg-gray-500 rounded-xl rounded-tl-none rounded-bl-none relative overflow-hidden">
-          <Image src="/foto-1.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-1.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
 
         <div className="bg-gray-500 row-span-2 rounded-xl relative overflow-hidden">
-          <Image src="/foto-3.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-3.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
 
         <div className="bg-gray-500 col-span-2 rounded-xl rounded-tr-none rounded-br-none relative overflow-hidden">
-          <Image src="/foto-4.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-4.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
         <div className=" bg-gray-500 rounded-xl rounded-tl-none rounded-bl-none relative overflow-hidden">
-          <Image src="/foto-2.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-2.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
         <div className="bg-gray-500 rounded-xl relative overflow-hidden">
-          <Image src="/foto-5.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-5.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
         <div className="bg-gray-500 rounded-xl rounded-tr-none rounded-br-none relative overflow-hidden">
-          <Image src="/foto-6.png" alt="Foto 1" fill className="object-cover" />
+          <Image
+            src="/foto-6.png"
+            alt="Foto 1"
+            fill
+            className="object-cover scale-105"
+          />
         </div>
       </div>
     </section>
