4f9a056193bedae241ef1f9a63a847c2a5c463ea
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-08-20 18:26:18 -0300

    Video set

--- src/templates/home/sections/hero-section/hero-section.tsx
+++ src/templates/home/sections/hero-section/hero-section.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 export function HeroSection() {
   return (
-    <section className="relative h-[80vh] sm:min-h-screen sm:h-full flex items-center justify-center">
-      <Image
-        src="/hero.jpg"
-        fill
-        alt="Hero Image"
-        className=" bg-cover bg-right bg-no-repeat brightness-75"
-        quality={100}
-      />
+    <section className="relative h-[80vh] sm:min-h-[90vh] flex items-center justify-center bg-black">
+      <video
+        src="/pmh-site-video.mp4"
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+      ></video>
     </section>
   );
 }
