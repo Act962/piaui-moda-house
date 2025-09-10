58d85996bc1223f6fc00b2eb163a437f557eb16d
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-09-10 12:09:54 -0300

    Teste posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -49,6 +49,7 @@ async function getData() {
 
 export default async function BlogPage() {
   const data: BasicBlogPost[] = await getData();
+  console.log(data);
 
   return (
     <div className="min-h-screen flex flex-col items-center">
