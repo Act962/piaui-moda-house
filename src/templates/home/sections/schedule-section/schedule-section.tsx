d736edc9eaed0e1afc01e66d38e2a9a1cb9ca8c4
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-10-07 14:58:17 -0300

    Update Schedule

--- src/templates/home/sections/schedule-section/schedule-section.tsx
+++ src/templates/home/sections/schedule-section/schedule-section.tsx
@@ -27,6 +27,16 @@ const schedules = [
     description:
       "CCT Sebrae Moda + PMH Business\n\nPROGRAMAÇÃO\n•	Almoço de Networking – Negócios de Moda\n•	Desfiles\n•	Palestras com nomes nacionais\n•	Talk PMH – com Raquel Dias e grandes nomes da moda piauiense\n•	Encerramento com atração musical no Grande Hall",
   },
+  {
+    date: "03 de novembro",
+    location: "Piauí - PI",
+    description: "PMH BUSINESS/ CCT",
+  },
+  {
+    date: "04 de novembro",
+    location: "Piauí - PI",
+    description: "DESFILE CONCEITUAL/ ESTAÇÃO IPHAN",
+  },
 ];
 
 export function ScheduleSection() {
