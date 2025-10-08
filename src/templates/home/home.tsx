5a886a7bc786e5a3825e0593b18d0d526e72a917
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-10-08 13:08:27 -0300

    remove schedule

--- src/templates/home/home.tsx
+++ src/templates/home/home.tsx
@@ -16,7 +16,7 @@ export function HomePage() {
     <>
       <HeroSection />
       <AboutSection />
-      <ScheduleSection />
+      {/* <ScheduleSection /> */}
       <OtherSection />
       <PartnerSection />
       <MadeSection />
